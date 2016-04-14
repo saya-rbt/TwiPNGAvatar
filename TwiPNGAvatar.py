748bfcccb2d8ab6b0c0ab964cff14a9c4d3fa42c
Skyneo <skyneo@live.fr>
2016-04-14 13:28:36 +0200

    Bugfix for windows cmd
    It worked perfectly with IEP (my IDE) but not with Windows' cmd. So I
    made a few changes and now it works perfectly.

--- TwiPNGAvatar.py
+++ TwiPNGAvatar.py
@@ -10,7 +10,7 @@ auth=tweepy.OAuthHandler("3wv3VFOZkEH1QVCfd0KHljKXV","15WqohzK9TDYgxIr4WTxLeJb32
 print("Go there and paste the PIN :")
 url = auth.get_authorization_url()
 print(url)
-pin = input("PIN: ")
+pin = str(input("PIN: "))
 
 # GET THE TOKEN
 auth.get_access_token(pin)
@@ -20,5 +20,5 @@ api = tweepy.API(auth)
 
 # Gets the path of the image, uploads it, and prints a confirmation message.
 path=input("Name of the image: ")
-api.update_profile_image(path)
+api.update_profile_image("./"+path)
 print("PP changed!")
